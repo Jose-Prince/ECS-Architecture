830ecf0973711918a2cdd3e2ae671509dd544e20
Jose-Prince <joseprince148@gmail.com>
2026-07-30 18:30:12 -0600

    feat: scene logic added

---
+++ ECS/Scene.lua
@@ -0,0 +1,31 @@
+Scene = Object.extend(Object)
+
+local Registry = require("ECS.registry")
+
+function Scene:new(index)
+    self.index = index
+    self.registry = Registry()
+    self.systems = {}
+end
+
+function Scene:addSystem(system)
+    table.insert(self.systems, system)    
+end
+
+function Scene:update(dt)
+    for _, system in ipairs(self.systems) do
+        if system.update then
+            system:update(self.registry, dt)
+        end
+    end
+end
+
+function Scene:draw()
+    for _, system in ipairs(self.system) do
+        if system.draw then
+            system:draw(self.registry)
+        end
+    end
+end
+
+return Scene
