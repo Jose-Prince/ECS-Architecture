bbd1a2d14aa96fd964ca6db70fd11897f3511bc8
Jose-Prince <joseprince148@gmail.com>
2026-08-13 21:37:56 -0600

    feat: show controls and entities

--- src/Editor/Editor.lua
+++ src/Editor/Editor.lua
@@ -19,6 +19,8 @@ function Editor:draw()
     end
 
     self:drawHeader()
+    self:drawControls()
+    self:drawEntities()
 end
 
 function Editor:drawHeader()
@@ -34,6 +36,67 @@ function Editor:drawHeader()
     })
 
     imlove.Text(self.scene.name)
+
+    imlove.End()
+end
+
+function Editor:drawControls()
+    local width = love.graphics.getWidth()
+
+    imlove.SetNextWindowPos(0, 50)
+    imlove.SetNextWindowSize(width, 50)
+
+    imlove.Begin("Editor Controls", nil, {
+        "NoTitleBar",
+        "NoMove",
+        "NoResize",
+        "NoCollapse"
+    })
+
+    if imlove.Button("Play") then
+        print("Play")
+    end
+
+    imlove.SameLine()
+
+    if imlove.Button("Pause") then
+        print("Pause")
+    end
+
+    imlove.SameLine()
+
+    if imlove.Button("Step") then
+        print("Step")
+    end
+
+    imlove.SameLine()
+
+    if imlove.Button("Reset") then
+        print("Reset")
+    end
+
+    imlove.End()
+end
+
+function Editor:drawEntities()
+    local height = love.graphics.getHeight()
+
+    imlove.SetNextWindowPos(0, 100)
+    imlove.SetNextWindowSize(200, height - 100)
+
+    imlove.Begin("Entities", nil, {
+        "NoMove",
+        "NoResize",
+        "NoCollapse"
+    })
+
+    if self.registry then
+        for entity, name in pairs(self.registry.entities) do
+            if imlove.Selectable(name) then
+                self.selectedEntity = entity
+            end
+        end
+    end
 
     imlove.End()
 end
